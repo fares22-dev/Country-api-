326a41e7d063048e6a0237141e08daf7633c42a8
Slimani Fares <124141004+fares22-dev@users.noreply.github.com>
2023-08-25 03:35:25 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App( ){
  
 
     useEffect(()=>{
-     fetch("http://localhost:5173/data.json ")
+     fetch("./data.json ")
      .then((response) => {
          if (!response.ok) {
            throw new Error('Network response was not ok');
